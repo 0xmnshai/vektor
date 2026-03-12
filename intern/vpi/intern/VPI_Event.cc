f5ae2720a1ded2c1649ad49d00957e246f72b4d1
Anshuman Tiwari <lazycodebaker@outlook.com>
2026-03-12 16:50:21 +0530

    event consume implemented

--- intern/vpi/intern/VPI_Event.cc
+++ intern/vpi/intern/VPI_Event.cc
@@ -1,9 +1,22 @@
 #include "VPI_Event.hh"
+#include "../../intern/clog/CLG_log.h"
 
 namespace vpi {
+
+CLG_LOGREF_DECLARE_GLOBAL(Event, "vpi.event");
+
 [[nodiscard]] VPI_TSuccess VPI_Event::consume() const noexcept
 {
+  if (consumed_) {
+    CLOG_WARN(Event, "Attempted to consume already-consumed event (type=%d)", (int)type_);
+    return VPI_kSuccess;
+  }
+
+  consumed_ = true;
+
+  CLOG_INFO(Event, "Event consumed (type=%d)", (int)type_);
+
   return VPI_kSuccess;
-};
+}
 
 }  // namespace vpi
