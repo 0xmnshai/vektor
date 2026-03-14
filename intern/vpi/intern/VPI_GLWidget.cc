c9ea13b0f9a17979fce32e3cc315e8e30e54be0a
Anshuman Tiwari <lazycodebaker@outlook.com>
2026-03-14 20:49:57 +0530

    widget class fix

--- intern/vpi/intern/VPI_GLWidget.cc
+++ intern/vpi/intern/VPI_GLWidget.cc
@@ -6,7 +6,7 @@ void VPI_GLWidget::initializeGL() {}
 // we can shift widget specific things to specific widget class [name].cc
 void VPI_GLWidget::resizeGL(int width, int height) {}
 
-void VPI_GLWidget::paintGL() {} 
+void VPI_GLWidget::paintGL() {}
 
 void VPI_GLWidget::mousePressEvent(QMouseEvent *event) {}
 
@@ -17,4 +17,9 @@ void VPI_GLWidget::keyPressEvent(QKeyEvent *event) {}
 void VPI_GLWidget::keyReleaseEvent(QKeyEvent *event) {}
 
 void VPI_GLWidget::wheelEvent(QWheelEvent *event) {}
+
+bool VPI_GLWidget::event(QEvent *event)
+{
+  return QOpenGLWidget::event(event);
 }
+}  // namespace vpi
